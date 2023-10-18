5ea353e32d67b911dd0a28d4513bbbc8f9be8e68
LucasReinert <lucotejra@gmail.com>
2023-10-18 17:10:05 -0300

    Adicionando conteúdo na SchedulingScreen

--- src/screens/SchedulingScreen.jsx
+++ src/screens/SchedulingScreen.jsx
@@ -4,7 +4,16 @@ export default function SchedulingScreen(){
         <div>
             <h1>Informações para o Agendamento:</h1>
             <ul>
-                <li>Nome:</li>
+                <li>Nome: <input type="text"/> </li>
+                <li>Código:</li>
+                <li>Data do atendimento:</li>
+                <li>Horário do atendimento:</li>
+            </ul>
+            <h1>Qual serviço deseja?</h1>
+            <ul>
+                <li>Mão e Pé</li>
+                <li>Cílios e obrancelha:</li>
+                <li>Depilação</li>
             </ul>
         </div>
     );
